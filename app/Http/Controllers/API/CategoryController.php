659a7840d6c2aab18f4fa8fae3839eea3f544e29
yasmen-ahmed <yasmenahmedmuhamed@gmail.com>
2023-06-26 18:14:52 +0300

    add new function

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return response()->json($categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $category = new Category();
+        $category->name = $request->input('name');
+        $category->save();
+        return response()->json($category);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $category = Category::find($id);
+        return response()->json($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $category = Category::find($id);
+        $category->name = $request->input('name');
+        $category->save();
+        return response()->json($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $category = Category::find($id);
+        $category->delete();
+        return response()->json(['message' => 'Category deleted successfully']);
+        
+    }
+}
---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // $posts = Post::all();
+        // return response()->json($posts);
+        $results = Post::with('category')
+        ->get();
+
+
+        return response()->json($results);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        // $post->category_id = $request->input('category_id');
+        $post->save();
+        return response()->json($post,201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = Post::find($id);
+        return response()->json($post);
+
+       
+
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->category_id = $request->input('category_id');
+        $post->save();
+        return response()->json($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $post = Post::find($id);
+        $post->delete();
+        return response()->json(['message' => 'Post deleted successfully']);
+    }
+
+    public function search(Request $request){
+        $query = $request->input('q');
+        $posts = Post::where('title', 'LIKE', "%$query%")
+                     ->orWhere('content', 'LIKE', "%$query%")
+                     ->get();
+        return response()->json($posts);
+    }
+    
+}
